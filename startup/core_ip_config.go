20dd15c635716fbd9256b3b2999523c8f3b900a2
ga111o <ga111o@proton.me>
2025-07-08 20:55:28 +0900

    🔥 Del deprecated config.yaml and streamline config handling in core_ip_config.go

--- startup/core_ip_config.go
+++ startup/core_ip_config.go
@@ -2,50 +2,20 @@ package startup
 
 import (
 	"fmt"
-	"io"
 	"os"
 
 	"github.com/easy-cloud-Knet/KWS_Control/structure"
+	"github.com/easy-cloud-Knet/KWS_Control/util"
 	"gopkg.in/yaml.v3"
 )
 
-const defaultConfigPath = "resources/config.yaml"
-
 func readConfig(path string) (structure.Config, error) {
+	log := util.GetLogger()
+
 	file, err := os.Open(path)
 	if err != nil {
-		if os.IsNotExist(err) {
-			srcFile, err := os.Open(defaultConfigPath)
-			if err != nil {
-				return structure.Config{}, fmt.Errorf("failed to open default config file '%s': %w", defaultConfigPath, err)
-			}
-			//goland:noinspection GoUnhandledErrorResult
-			defer srcFile.Close()
-
-			destFile, err := os.Create(path)
-			if err != nil {
-				return structure.Config{}, fmt.Errorf("failed to create config file '%s': %w", path, err)
-			}
-			//goland:noinspection GoUnhandledErrorResult
-			defer destFile.Close()
-
-			_, err = io.Copy(destFile, srcFile)
-			if err != nil {
-				return structure.Config{}, fmt.Errorf("failed to copy default config to '%s': %w", path, err)
-			}
-
-			if err := destFile.Close(); err != nil {
-				return structure.Config{}, fmt.Errorf("failed to close newly created config file '%s': %w", path, err)
-			}
-
-			file, err = os.Open(path)
-			if err != nil {
-				return structure.Config{}, fmt.Errorf("failed to open newly created config file '%s': %w", path, err)
-			}
-		} else {
-			// Other error occurred when trying to open the file initially
-			return structure.Config{}, fmt.Errorf("failed to open config file '%s': %w", path, err)
-		}
+		log.Errorf("failed to open config file: %v", err)
+		return structure.Config{}, fmt.Errorf("failed to open config file: %w", err)
 	}
 
 	//goland:noinspection GoUnhandledErrorResult
